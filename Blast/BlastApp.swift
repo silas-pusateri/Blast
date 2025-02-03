b30c311e5e3bc5559eaa0173dab54646327161aa
Silas Pusateri <smphockey7@gmail.com>
2025-02-03 12:46:58 -0600

    Initial Commit

---
+++ Blast/BlastApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlastApp.swift
+//  Blast
+//
+//  Created by Silas Pusateri on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct BlastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlastTests/BlastTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlastTests.swift
+//  BlastTests
+//
+//  Created by Silas Pusateri on 2/3/25.
+//
+
+import Testing
+@testable import Blast
+
+struct BlastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
