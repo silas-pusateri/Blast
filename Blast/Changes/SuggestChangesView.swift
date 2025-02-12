42455971548e21e9f754548856e3a29adeddd603
Silas Pusateri <smphockey7@gmail.com>
2025-02-11 21:49:25 -0600

    Refactor VESDK for CESDK Video Editor to remove watermark

--- Blast/Changes/SuggestChangesView.swift
+++ Blast/Changes/SuggestChangesView.swift
@@ -126,6 +126,7 @@ struct SuggestChangesView: View {
                 self.editedVideoURL = url
                 self.editMetadata = metadata
             }
+            .interactiveDismissDisabled()
         }
     }
     
